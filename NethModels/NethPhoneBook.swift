23d2b07e2e96eedcf1bfb1782fb967a71597f5cb
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-02-10 15:47:04 +0100

    Improve phonebook store.

--- NethModels/NethPhoneBook.swift
+++ NethModels/NethPhoneBook.swift
@@ -8,17 +8,48 @@
 import Foundation
 import linphonesw
 
+/**
+ Singleton store of number of phonebook rows already downloaded from remote phonebook and max contacts to download.
+ */
 class NethPhoneBook: NSObject {
-    var map: Dictionary<String, NethContact>
+    var count: Int
+    var rows: Int
     
     fileprivate override init() {
-        map = Dictionary<String, NethContact>()
+        count = 0;
+        rows = 0;
     }
+    
     private static var _singleton: NethPhoneBook?
+    
     @objc public class func instance() -> NethPhoneBook {
         if _singleton == nil {
             _singleton = NethPhoneBook()
         }
         return _singleton!
+    }
+    
+    /**
+     Return true if api can return more results.
+     
+     If rows is equal or more the count of phonebook contact, no more contacts need to be downloaded.
+     */
+    @objc public func hasMore(_ index: Int) -> Bool {
+        return rows < count && index + 20 > rows
+    }
+    
+    /**
+     Increment the number of contacts loaded.
+     */
+    @objc public func load(_ number: Int, max: Int) -> Void {
+        rows += number;
+        count = max;
+    }
+    
+    /**
+     Reset current max number of phonebook contacts.
+     */
+    @objc public func reset() -> Void {
+        rows = 0;
     }
 }
