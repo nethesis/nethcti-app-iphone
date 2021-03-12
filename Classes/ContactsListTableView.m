73396e1ac0a9be4253351a423fe5de422a31c71b
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-03-12 18:00:57 +0100

    Ordering workaround.

--- Classes/ContactsListTableView.m
+++ Classes/ContactsListTableView.m
@@ -143,16 +143,15 @@ static int ms_strcmpfuz(const char *fuzzy_word, const char *sentence) {
 		@synchronized(myMap) {
 			NSDictionary *allContacts = [[NSMutableDictionary alloc] initWithDictionary:LinphoneManager.instance.fastAddressBook.addressBookMap];
             // Those are only sip addresses sorted by first and last name.
-            sortedAddresses = [[LinphoneManager.instance.fastAddressBook.addressBookMap allKeys] sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
+            sortedAddresses = [ContactSelection getSipFilter] && ![[ContactSelection getNameOrEmailFilter] isEqual: @""] ? [LinphoneManager.instance.fastAddressBook.addressBookMap allKeys] : [[LinphoneManager.instance.fastAddressBook.addressBookMap allKeys] sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
                 Contact* first =  [allContacts objectForKey:a];
                 Contact* second =  [allContacts objectForKey:b];
                 // if([[ContactSelection getPickerFilter] isEqual:@"all"]) {
-                    // return [[first.company lowercaseString] compare:[second.company lowercaseString]];
+                // return [[first.company lowercaseString] compare:[second.company lowercaseString]];
                 // } else {
-                    if([[first.firstName lowercaseString] compare:[second.firstName lowercaseString]] == NSOrderedSame)
-                        return [[first.lastName lowercaseString] compare:[second.lastName lowercaseString]];
-                    else
-                        return [[first.firstName lowercaseString] compare:[second.firstName lowercaseString]];
+                if([[first.firstName lowercaseString] compare:[second.firstName lowercaseString]] == NSOrderedSame)
+                    return [[first.lastName lowercaseString] compare:[second.lastName lowercaseString]];
+                return [[first.firstName lowercaseString] compare:[second.firstName lowercaseString]];
                 // }
             }];
 			
@@ -184,7 +183,7 @@ static int ms_strcmpfuz(const char *fuzzy_word, const char *sentence) {
                 
                 if ([FastAddressBook contactHasValidSipDomain:contact]) {
                     add = true;
-                }else if (contact.friend &&
+                } else if (contact.friend &&
                     linphone_presence_model_get_basic_status(
                         linphone_friend_get_presence_model(contact.friend)) == LinphonePresenceBasicStatusOpen) {
                     add = true;
@@ -200,15 +199,32 @@ static int ms_strcmpfuz(const char *fuzzy_word, const char *sentence) {
 
                 NSMutableString *name = [[NSMutableString alloc] initWithString:@""];
                 // if(![ContactSelection getSipFilter] && !contact.nethesis) {
-                    name = [self displayNameForContact:contact] ? [[NSMutableString alloc] initWithString: [self displayNameForContact:contact]] : nil;
+                name = [self displayNameForContact:contact] ? [[NSMutableString alloc] initWithString: [self displayNameForContact:contact]] : nil;
                 // } else if(contact.company && contact.company.length > 0) {
-                    // name = [[NSMutableString alloc] initWithString: contact.company];
+                // name = [[NSMutableString alloc] initWithString: contact.company];
                 // } else {
-                    // Same as first.
-                    // name = [self displayNameForContact:contact] ? [[NSMutableString alloc] initWithString: [self displayNameForContact:contact]] : nil;
+                // Same as first.
+                // name = [self displayNameForContact:contact] ? [[NSMutableString alloc] initWithString: [self displayNameForContact:contact]] : nil;
                 // }
                 
-                if (add && name != nil) {
+                if([ContactSelection getSipFilter] && ![[ContactSelection getNameOrEmailFilter] isEqual: @""] && add) {
+                    NSMutableArray *subAr = [myMap objectForKey:@"A"];
+                    if (subAr == nil) {
+                        subAr = [[NSMutableArray alloc] init];
+                        [myMap insertObject:subAr forKey:@"A" selector:@selector(caseInsensitiveCompare:)];
+                    }
+                    
+                    NSUInteger idx = [subAr indexOfObject:contact inSortedRange:(NSRange){0, subAr.count} options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult( Contact *_Nonnull obj1, Contact *_Nonnull obj2) {
+                        if([[obj1.company lowercaseString] compare:[obj2.company lowercaseString]] == NSOrderedSame)
+                            return [[[self displayNameForContact:obj1] lowercaseString] compare:[[self displayNameForContact:obj2] lowercaseString]];
+                        else
+                            return [[obj1.company lowercaseString] compare:[obj2.company lowercaseString]];
+                    }];
+                    
+                    if (![subAr containsObject:contact]) {
+                        [subAr insertObject:contact atIndex:idx];
+                    }
+                } else if (add && name != nil) {
                     NSString *firstChar = [[name substringToIndex:1] uppercaseString];
                     // Put in correct subAr
                     if ([firstChar characterAtIndex:0] < 'A' || [firstChar characterAtIndex:0] > 'Z') {
@@ -221,7 +237,7 @@ static int ms_strcmpfuz(const char *fuzzy_word, const char *sentence) {
                     }
                     NSUInteger idx = [subAr indexOfObject:contact inSortedRange:(NSRange){0, subAr.count} options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult( Contact *_Nonnull obj1, Contact *_Nonnull obj2) {
                         // if([[obj1.company lowercaseString] compare:[obj2.company lowercaseString]] == NSOrderedSame)
-                            return [[[self displayNameForContact:obj1] lowercaseString] compare:[[self displayNameForContact:obj2] lowercaseString]];
+                        return NSOrderedAscending; // [[[self displayNameForContact:obj1] lowercaseString] compare:[[self displayNameForContact:obj2] lowercaseString]];
                         // else
                             // return [[obj1.company lowercaseString] compare:[obj2.company lowercaseString]];
                     }];
